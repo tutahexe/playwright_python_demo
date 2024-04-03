5286ef0006753589dc62479d3f72cdbada749961
Mike <againstthesilence@gmail.com>
2024-04-03 14:43:54 +0300

    Basic test initial implementation

---
+++ test_authorization.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page
+
+
+def test_successful_auth(page: Page):
+    page.goto("https://www.saucedemo.com/")
+    page.locator("#user-name").fill("standard_user")
+    page.locator("#password").fill("secret_sauce")
+    page.locator("#login-button").click()
+    assert page.locator("#react-burger-menu-btn").is_visible() is True
