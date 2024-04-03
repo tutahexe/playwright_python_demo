311b13bd9b6ab405bd9834160b01fb9a360f54e9
Mike <againstthesilence@gmail.com>
2024-04-03 18:23:04 +0300

    Added couple negative authorization

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -3,3 +3,4 @@ class LoginPage:
         self.user_name = ui.page.locator("#user-name")
         self.password = ui.page.locator("#password")
         self.login_button = ui.page.locator("#login-button")
+        self.error_label = ui.page.locator("xpath=//h3[@data-test='error']")
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,5 +4,19 @@ import pytest
 @pytest.mark.ui
 def test_successful_auth(ui):
     ui.steps.authorization.login_with_user(ui.user, ui.password)
-    assert ui.pages.main_page.menu_button.is_visible() is True and ui.pages.login_page.user_name.is_hidden() is True, \
-        "Failed to authorize "
+    assert ui.pages.main_page.menu_button.is_visible() is True \
+           and ui.pages.login_page.user_name.is_hidden() is True, "Failed to authorize "
+
+
+@pytest.mark.ui
+def test_failed_auth(ui):
+    ui.steps.authorization.login_with_user(ui.user, "wrong_password")
+    assert ui.pages.login_page.error_label.is_visible() is True \
+           and ui.pages.login_page.user_name.is_visible() is True, "Failed to authorize "
+
+
+@pytest.mark.ui
+def test_failed_auth_label(ui):
+    ui.steps.authorization.login_with_user(ui.user, "wrong_password")
+    assert ui.pages.login_page.error_label.text_content() == \
+           "Epic sadface: Username and password do not match any user in this service"
