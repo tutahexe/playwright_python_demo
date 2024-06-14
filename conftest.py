9e782ad2f03bb47110a94ccc65db759ced2f597f
Mike <againstthesilence@gmail.com>
2024-06-14 11:36:56 +0300

    env fixture created

--- conftest.py
+++ conftest.py
@@ -5,17 +5,18 @@ import pytest
 from playwright.sync_api import Page, BrowserContext
 from core.ui_facade import UI
 
-env_target = None
-
 
 @pytest.fixture(scope="function")
-def ui(page: Page, context: BrowserContext):
-    global env_target
-    if env_target is None:
-        config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                        ".env")
-        with open(config_file_path) as config:
-            env_target = json.load(config)
-    ui_item = UI(page, context, user=env_target['user'], password=env_target['password'], url=env_target['url'])
-    ui_item.page.goto(env_target['url'])
+def ui(page: Page, context: BrowserContext, env):
+    ui_item = UI(page, context, user=env['user'], password=env['password'], url=env['url'])
+    ui_item.page.goto(env['url'])
     return ui_item
+
+
+@pytest.fixture(scope="session")
+def env():
+    config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                    ".env")
+    with open(config_file_path) as config:
+        env_target = json.load(config)
+    return env_target
